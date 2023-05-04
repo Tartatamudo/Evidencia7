ae7e26cf33cf84100d90aa32d3b03efec2cc4d1a
Tartatamudo <fabianalarcon123@gmail.com>
2023-05-04 19:54:38 -0400

    Aqui van al commit la clase Calculadora y la clase CarroCompra dentro del paquete package como lo hice en visual Paradigm

---
+++ src/main/java/Package/Calculadora.java
@@ -0,0 +1,43 @@
+package Package;
+
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param n1
+	 * @param n2
+	 */
+	public Calculadora(int n1, int n2) {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	public int Sumar() {
+		// TODO - implement Calculadora.Sumar
+		throw new UnsupportedOperationException();
+	}
+
+	public int Multiplicar() {
+		// TODO - implement Calculadora.Multiplicar
+		throw new UnsupportedOperationException();
+	}
+
+	public void SetN1() {
+		// TODO - implement Calculadora.SetN1
+		throw new UnsupportedOperationException();
+	}
+
+	public void SetN2() {
+		// TODO - implement Calculadora.SetN2
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/Package/CarroCompra.java
@@ -0,0 +1,33 @@
+package Package;
+
+public class CarroCompra {
+
+	private int[][] productos;
+
+	public CarroCompra() {
+		// TODO - implement CarroCompra.CarroCompra
+		throw new UnsupportedOperationException();
+	}
+
+	private int CalcularTotal() {
+		// TODO - implement CarroCompra.CalcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	
+	private int SubTotal(int cant, int precio) {
+		// TODO - implement CarroCompra.SubTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void MostrarTotal() {
+		// TODO - implement CarroCompra.MostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
